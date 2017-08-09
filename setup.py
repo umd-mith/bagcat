44e506bc6c8ce34b29aa7747ef23c387f6be2c69
Ed Summers <ehs@pobox.com>
2017-08-09 13:38:34 -0400

    upped version for release to pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ requirements = open("requirements.txt").read().split()
 
 setup(
     name = 'bagcat',
-    version = '0.0.3',
+    version = '0.0.4',
     url = 'https://github.com/umd-mith/bagcat/',
     author = 'Ed Summers',
     author_email = 'ehs@pobox.com',

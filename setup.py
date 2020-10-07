03535b41ddc942092cefb938e8a049371ab946fd
Ed Summers <ehs@pobox.com>
2020-10-07 08:06:30 -0400

    create entrypoint

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 
 requirements = open("requirements.txt").read().split()
 
+with open("README.md") as f:
+    long_description = f.read()
+
 setup(
     name = 'bagcat',
     version = '0.0.6',
@@ -10,6 +13,9 @@ setup(
     author = 'Ed Summers',
     author_email = 'ehs@pobox.com',
     py_modules = ['bagcat',],
-    scripts = ['scripts/bagcat'],
     install_requires = requirements,
-    description = "A command line utility for managing BagIt packages in Amazon S3")
+    description = "A command line utility for managing BagIt packages in Amazon S3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    entry_points={"console_scripts": ["bagcat=bagcat:main"]},
+)
